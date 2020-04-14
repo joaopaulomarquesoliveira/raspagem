d970c0f7250662a8d1d6d58b3ad524ef36bd5822
juacy <56744872+juacy@users.noreply.github.com>
2020-04-14 17:02:31 -0300

    Atualização
    Adicionado ao código a automatização para que o script rode em todos os arquivos de uma pasta

--- Raspagem.py
+++ Raspagem.py
@@ -3,9 +3,8 @@ import re
 import os
 import unidecode
 
-
-#carregando o documento
-documento = Document()
+os.chdir(r'') #colocar o caminho para a pasta com os arquivos que serão raspados
+lista_de_arquivos = os.listdir(os.getcwd())
 
 def EncontraArma(marcadores, texto):
     arma = []
@@ -89,61 +88,64 @@ def SelecionarParagrafos( texto, localizacao):
         localizacao = localizacao + 1
     return salvar
 
-Dados =  {}
-Marcadores = []
-Infos = []
-paragrafos=[] 
-
-#filtrando somente os textos do documetno
-paragrafos = FormataTexto(FiltraTexto(documento))
-Marcadores = DefineMarcadores(paragrafos)
-        
-for num_para in range(0, len(paragrafos)):
-
-### busca os campos dentro do texto
-    if (re.search(r'aos \d{2} de \w* de \d{4}', paragrafos[num_para])):
-        Dados['data'] = re.search(r'aos \d{2} de \w* de \d{4}', paragrafos[num_para]).group(0)[4:]
-
-    if (re.search(r', fo\w* designad\w* \w* perit(.*) para', paragrafos[num_para])):
-        Dados['perito'] = re.search(r'.erit(.*) para', paragrafos[num_para]).group(0)#[:-4]
-        
-    if (re.search(r'proceder \w* exame \w* (.*)', paragrafos[num_para])):
-        Dados['tipo_laudo'] = re.search(r'proceder \w* exame \w* (.*) a fim', paragrafos[num_para]).group(0)
-        
-    if (re.search(r'meio d[ao](.*)protocolado', paragrafos[num_para])):
-        Dados['oficio'] = re.search(r'meio d[ao](.*)protocolado', paragrafos[num_para]).group(0)[7:-12]
-        
-    if (re.search(r'sob o numero (.*)', paragrafos[num_para])):
-        Dados['protocolo'] = re.search(r'sob o numero (.*)', paragrafos[num_para]).group(0)[13:]
-        
-    if (re.search(r'ref.(.*)',paragrafos[num_para])):
-        Dados['ref_oficio'] = re.search(r'ref.(.*)',paragrafos[num_para]).group(0)[3:]
-        
-    if (re.search(r'laudo no.*', paragrafos[num_para])):
-        Dados['num_laudo'] = re.search(r'laudo no(.*)', paragrafos[num_para]).group(0) 
-
-#### utilizam funcao para saber ate onde vai o texto    
+for arquivo in lista_de_arquivos:
+    documento = Document(arquivo)
     
-    if (re.search(r'i - historico', paragrafos[num_para])):
-        Dados['historico'] =  PegaTodaTexto(paragrafos, num_para, Marcadores)
+    Dados =  {}
+    Marcadores = []
+    Infos = []
+    paragrafos=[] 
+
+    #filtrando somente os textos do documetno
+    paragrafos = FormataTexto(FiltraTexto(documento))
+    Marcadores = DefineMarcadores(paragrafos)
         
-    if (re.search(r'da efici\wncia',paragrafos[num_para])):
-        Dados['eficiencia'] = PegaTodaTexto(paragrafos, num_para, Marcadores)
+    for num_para in range(0, len(paragrafos)):
 
-    if (re.search(r'de outros elementos:', paragrafos[num_para])):
-        Dados['uso'] = paragrafos[num_para + 1] 
+    ### busca os campos dentro do texto
+        if (re.search(r'aos \d{2} de \w* de \d{4}', paragrafos[num_para])):
+            Dados['data'] = re.search(r'aos \d{2} de \w* de \d{4}', paragrafos[num_para]).group(0)[4:]
 
-    if (re.search(r'fo\w* encaminhad\w* \w* .* mater\w*:', paragrafos[num_para])):
-        Dados['material'] = PegaTodaTexto(paragrafos, num_para, Marcadores)
+        if (re.search(r', fo\w* designad\w* \w* perit(.*) para', paragrafos[num_para])):
+            Dados['perito'] = re.search(r'.erit(.*) para', paragrafos[num_para]).group(0)#[:-4]
+        
+        if (re.search(r'proceder \w* exame \w* (.*)', paragrafos[num_para])):
+            Dados['tipo_laudo'] = re.search(r'proceder \w* exame \w* (.*) a fim', paragrafos[num_para]).group(0)
+        
+        if (re.search(r'meio d[ao](.*)protocolado', paragrafos[num_para])):
+            Dados['oficio'] = re.search(r'meio d[ao](.*)protocolado', paragrafos[num_para]).group(0)[7:-12]
+        
+        if (re.search(r'sob o numero (.*)', paragrafos[num_para])):
+            Dados['protocolo'] = re.search(r'sob o numero (.*)', paragrafos[num_para]).group(0)[13:]
+        
+        if (re.search(r'ref.(.*)',paragrafos[num_para])):
+            Dados['ref_oficio'] = re.search(r'ref.(.*)',paragrafos[num_para]).group(0)[3:]
+        
+        if (re.search(r'laudo no.*', paragrafos[num_para])):
+            Dados['num_laudo'] = re.search(r'laudo no(.*)', paragrafos[num_para]).group(0) 
 
-    if (re.search(r'\Aiii .* conclusao:(.*)', paragrafos[num_para])):
-        Dados['conclusao'] = PegaTodaTexto(paragrafos, num_para, Marcadores)
+            #### utilizam funcao para saber ate onde vai o texto    
+    
+        if (re.search(r'i - historico', paragrafos[num_para])):
+            Dados['historico'] =  PegaTodaTexto(paragrafos, num_para, Marcadores)
+        
+        if (re.search(r'da efici\wncia',paragrafos[num_para])):
+            Dados['eficiencia'] = PegaTodaTexto(paragrafos, num_para, Marcadores)
+
+        if (re.search(r'de outros elementos:', paragrafos[num_para])):
+            Dados['uso'] = paragrafos[num_para + 1] 
+
+        if (re.search(r'fo\w* encaminhad\w* \w* .* mater\w*:', paragrafos[num_para])):
+            Dados['material'] = PegaTodaTexto(paragrafos, num_para, Marcadores)
+
+        if (re.search(r'\Aiii .* conclusao:(.*)', paragrafos[num_para])):
+            Dados['conclusao'] = PegaTodaTexto(paragrafos, num_para, Marcadores)
 
 
 
-## atribuindo as informacoes das armas
-Dados.update(EncontraArma(Marcadores))
+    ## atribuindo as informacoes das armas
+    Dados.update(EncontraArma(Marcadores))
 
-### mostrando os dados:
-for key, value in Dados.items():
-    print(key, ' : ', value,'\n')
+    ### mostrando os dados:
+    for key, value in Dados.items():
+        print(key, ' : ', value,'\n')
